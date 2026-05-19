21bc70904285fe5788aae598e1ef3a6e9c94856e
Anirudh Nayak <anirudhsnayak@gmail.com>
2026-05-19 01:41:06 -0700

    initial draft of wireframe UI

---
+++ src/final/js/ui/components/PromptDisplay.js
@@ -0,0 +1,28 @@
+import { assertHTMLElement } from "../utils.js";
+
+/**
+ * The component responsible for displaying the current prompt to the user.
+ * 
+ * Expects the following minimal HTML structure:
+ * <div class="prompt-display">
+ *   <p class="prompt-display-text"></p>
+ * </div>
+ */
+export default class PromptDisplay {
+    /**
+     * Binds this PromptDisplay to the given element.
+     * @param {HTMLElement} element 
+     */
+    constructor(element) {
+        this.element = element;
+        this.promptTextElement = assertHTMLElement(this.element.querySelector('.prompt-display-text'));
+    }
+
+    /**
+     * Sets the text of the prompt display.
+     * @param {String} text - The text to display.
+     */
+    setText(text) {
+        this.promptTextElement.textContent = text;
+    }
+}
