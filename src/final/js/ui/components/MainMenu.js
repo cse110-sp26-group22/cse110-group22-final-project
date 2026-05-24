5968ed1157f57573cb7375d919498bdbaaea24fe
Sofia <sofia.demarco.en@gmail.com>
2026-05-24 15:55:15 -0700

    added MainMenu js file

---
+++ src/final/js/ui/components/MainMenu.js
@@ -0,0 +1,42 @@
+import { assertHTMLElement } from "../utils.js";
+
+/**
+ * The main menu component for the game.
+ *
+ * Expects the following minimal HTML structure:
+ * <div class="main-menu">
+ *   <select class="main-menu-language-select">
+ *     <option value="python">Python</option>
+ *   </select>
+ *   <button type="button" class="main-menu-start">Start Game</button>
+ * </div>
+ */
+export default class MainMenu {
+    /** @type {((language: string) => void) | null} */
+    #startCallback = null;
+
+    /**
+     * Binds this MainMenu to the given element.
+     * @param {HTMLElement} element
+     */
+    constructor(element) {
+        /** @type {HTMLElement} */
+        this.element = element;
+        /** @private @type {HTMLSelectElement} */
+        this.languageSelect = assertHTMLElement(this.element.querySelector('.main-menu-language-select'));
+        /** @private @type {HTMLElement} */
+        this.startButton = assertHTMLElement(this.element.querySelector('.main-menu-start'));
+
+        this.startButton.addEventListener('click', () => {
+            if (this.#startCallback) this.#startCallback(this.languageSelect.value);
+        });
+    }
+
+    /**
+     * Registers a callback to be called when the user starts the game.
+     * @param {(language: string) => void} callback - Called with the selected language value.
+     */
+    onStart(callback) {
+        this.#startCallback = callback;
+    }
+}
