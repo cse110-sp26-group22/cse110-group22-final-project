9af891e7c7cef4f164d4d1e17cd79fcccf542748
Lucien <Lucien2714@gmail.com>
2026-06-04 02:58:18 -0700

    Fix: change in game stats display WPM to CPM for consistency in result screen

--- src/final/js/ui/components/game/StatsDisplay.js
+++ src/final/js/ui/components/game/StatsDisplay.js
@@ -6,7 +6,7 @@
  * <div class="stats-display">
  *   <p>Score: <span class="stats-display-score"></span></p>
  *   <p>Accuracy: <span class="stats-display-accuracy"></span></p>
- *   <p>WPM: <span class="stats-display-wpm"></span></p>
+ *   <p>CPM: <span class="stats-display-cpm"></span></p>
  * </div>
  */
 export default class StatsDisplay {
@@ -19,6 +19,16 @@ export default class StatsDisplay {
         this.element = element;
         this.scoreElement = this.element.querySelector('.stats-display-score');
         this.accuracyElement = this.element.querySelector('.stats-display-accuracy');
-        this.wpmElement = this.element.querySelector('.stats-display-wpm');
+        this.cpmElement = this.element.querySelector('.stats-display-cpm');
+    }
+
+    /**
+     * Updates the displayed gameplay stats.
+     * @param {{ score: number, accuracy: string, cpm: number }} stats
+     */
+    update(stats) {
+        this.scoreElement.textContent = `${stats.score}`;
+        this.accuracyElement.textContent = stats.accuracy;
+        this.cpmElement.textContent = `${stats.cpm}`;
     }
 }
