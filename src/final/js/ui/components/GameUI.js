a510661cf1b88774ee460bbc340710c3c7f4758c
Anirudh Nayak <anirudhsnayak@gmail.com>
2026-06-06 02:32:23 -0700

    fix: remove GameTray as it is obsolete

--- src/final/js/ui/components/GameUI.js
+++ src/final/js/ui/components/GameUI.js
@@ -4,7 +4,6 @@ import PromptDisplay from "./game/input/PromptDisplay.js";
 import StatsDisplay from "./game/StatsDisplay.js";
 import PauseMenu from "./game/PauseMenu.js";
 import PlantDisplayGroup from "./game/plants/PlantDisplayGroup.js";
-import GameTray from "./game/tray/GameTray.js";
 import Combo from "./game/Combo.js";
 import Timer from "./game/Timer.js";
 import NotificationDisplay from "./NotificationDisplay.js";
@@ -47,7 +46,6 @@ export default class GameUI {
         this.onPause(() => this.pauseMenu.show());
         this.onResume(() => this.pauseMenu.hide());
         
-        this.gameTray = new GameTray(assertHTMLElement(this.element.querySelector('.game-tray')));
         this.onClockTick(() => this.updateLiveData());
         this.onPause(() => console.log('game paused'));
     }
