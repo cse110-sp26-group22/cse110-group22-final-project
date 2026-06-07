09afc4cd256121e3ce9061a01295aae9f2c6c062
Angel Thakur <athakur@ucsd.edu>
2026-06-06 22:42:17 -0700

    chore: added config to vite to point in direction of index.html location

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // Tell Vite that project source files live in this subfolder
+  root: 'src/final/html',
+
+  // 2. Keep your base path for GitHub Pages
   base: '/cse110-group22-final-project/',
+  
   build: {
-    rollupOptions: {
-      input: './src/final/html/index.html' // or './src/sample/index.html' depending on which is your main entry
-    }
+    // Since the root is inside src/final/html, we need to go up 4 levels 
+    // to put the final bundled assets into the main root './dist' folder
+    outDir: '../../../../dist',
+    
+    // Clear the old dist folder before building new files
+    emptyOutDir: true,
   }
 });
