05801ece1174c21d153b4e9bcfca2c84f74ba69e
Dmitri <lakedre21@gmail.com>
2026-06-06 20:40:31 -0700

    Finished UI documentation, changed some of the notation for plantdisplay since it was about displaying a different component.

--- src/final/js/ui/components/game/plants/PlantDisplay.js
+++ src/final/js/ui/components/game/plants/PlantDisplay.js
@@ -1,24 +1,24 @@
 /**
- * The component responsible for displaying the user's current score and accuracy.
+ * The component responsible for displaying the user's current plant level.
  * 
  * Expects the following minimal HTML structure:
  * <div class="plant-display"></div>
  */
-export default class PlantDisplay  {
+export default class PlantDisplay {
     /** @const {(string)[]}*/
     static PLANT_IMAGES = ['plant1-stage1.png', 'plant1-stage2.png', 'plant1-stage3.png'];
     /**
      * Binds this PlantDisplay to the given element.
      * @param {HTMLElement} element 
      */
-    constructor(element){
+    constructor(element) {
         this.element = element;
         this.growthLevel = 0;
         this.plantImageElement = document.createElement('img');
 
         this.build();
     }
-    build(){
+    build() {
         this.plantImageElement.src = `../assets/images/plant/${PlantDisplay.PLANT_IMAGES[this.growthLevel]}`;
         this.plantImageElement.classList.add('plant-image');
         this.element.appendChild(this.plantImageElement);
